c2734110f773b1ad05a8b6e3279de23bfa3699c9
Shruti <shrutiperiwal21@gmail.com>
2022-05-19 12:56:32 +0530

    OOPs

--- JS/oop.js
+++ JS/oop.js
@@ -16,3 +16,23 @@ const shruti = new Person('Shruti', 2001);
 console.log(shruti);
 
 console.log(shruti instanceof Person);
+
+//Prototype 
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function() {
+    console.log(2022 - this.birthYear);
+};
+shruti.calcAge();
+
+console.log(shruti.__proto__);
+console.log(shruti.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(shruti));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(shruti.species);
+
+console.log(shruti.hasOwnProperty('species'));
+console.log(shruti.hasOwnProperty('firstName'));
