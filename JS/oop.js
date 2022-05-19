4202922ae09f76199a4ecd95d0454c4041259826
Shruti <shrutiperiwal21@gmail.com>
2022-05-19 18:11:04 +0530

    OOPs

--- JS/oop.js
+++ JS/oop.js
@@ -51,8 +51,8 @@ console.log(arr.unique());
 console.log('----------ES6 CLASSES----------');
 //class declarations
 class PersonCl {
-    constructor(firstName, birthYear) {
-        this.firstName = firstName;
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
         this.birthYear = birthYear;
     }
     //Methods will be added to .prototype property
@@ -60,14 +60,31 @@ class PersonCl {
         console.log(2022 - this.birthYear);
     }
     greet() {
-        console.log(`Hey ${this.firstName}`);
+        console.log(`Hey ${this.fullName}`);
+    }
+
+    get age() {
+        return 2022 - this.birthYear;
+    }
+
+    //Set a property that already exists
+    set fullName(name) {
+        if(name.includes(' ')) {
+            this._fullName = name;
+        } else {
+            console.log(`${name} is not a full name`);
+        }
+    }
+
+    get fullName() {
+        return this._fullName;
     }
 }
 
-const ShrutiPeriwal = new PersonCl('ShrutiPeriwal', 2001);
+const ShrutiPeriwal = new PersonCl('Shruti Periwal', 2001);
 console.log(ShrutiPeriwal);
 ShrutiPeriwal.calcAge();
-
+console.log(ShrutiPeriwal.age);
 //PersonCl.prototype.greet = function() {
 //    console.log(`Hey ${this.firstName}`);
 //}
@@ -76,3 +93,21 @@ ShrutiPeriwal.greet();
 //1.classes are not hoisted
 //2. Classes are also first-class citizens
 //3. Classes are exectuted in strict mode
+
+//Setters and Getters property
+console.log('--------Setters and Getters--------');
+const account = {
+    owner : 'shruti',
+    movements : [200, 530, 120, 300],
+
+    get latest() {
+        return this.movements.slice(-1).pop();
+    },
+
+    set latest(mov) {
+        this.movements.push(mov);
+    },
+};
+console.log(account.latest);
+account.latest = 5000;
+console.log(account.movements);
