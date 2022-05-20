88099aed618e30ec8b4192fc8537c8f545e5ccbc
Shruti <shrutiperiwal21@gmail.com>
2022-05-20 08:05:10 +0530

    Classes

--- JS/class.js
+++ JS/class.js
@@ -37,15 +37,19 @@ class Account {
         this.owner = owner;
         this.currency = currency;
         this.pin = pin;
-        this.movements = [];
+        this._movements = [];
       //  this.locale = navigator.language;
 
         console.log(`Thanks for opening an account, ${owner}`);
     }
 
 //Public interface
+getMovements() {
+    return this._movements;
+}
+
 deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
 }
 
 withdraw(val) {
@@ -69,6 +73,7 @@ acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
 acc1.approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
