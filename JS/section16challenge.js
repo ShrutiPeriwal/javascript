c1160c5ae550100f106e2ebb51e4995624c97e40
Shruti <shrutiperiwal21@gmail.com>
2022-05-24 11:13:01 +0530

    challenge

--- JS/section16challenge.js
+++ JS/section16challenge.js
@@ -73,3 +73,28 @@ const getJSON = function (url, errorMsg = "Something Went Wrong") {
   });
 
   lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+  //Promisifying setTimeout
+  const wait = function(seconds) {
+    return new Promise(function(resolve){
+      setTimeout(resolve, seconds * 1000);
+    });
+  };
+
+  wait(1)
+  .then(() => {
+    console.log('1 seconds passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 seconds passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 seconds passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 seconds passed'));
+
+  Promise.resolve('.abc').then(x => console.log(x));
+  Promise.reject(new Error('Problem!!')).catch(x => console.error(x));
