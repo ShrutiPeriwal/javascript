7e7081c2f0a7c0bcadb5adc220233e697290ba0d
Shruti <shrutiperiwal21@gmail.com>
2022-05-28 21:36:55 +0530

    LOOP

---
+++ JS/loops.js
@@ -0,0 +1,29 @@
+// Lecture number 26
+const myState = [
+    "Rajasthan" ,
+    "Delhi" ,
+    "Assam" ,
+    1947,
+    "Tamil Nadu" ,
+    "Maharastra" ,
+];
+//for(let i = 0; i < myState.length; i++) {
+  //  if( typeof myState[i] !== "string") 
+  //      break;
+  //  console.log(myState[i]); //rajasthan , delhi , Assam
+//}
+
+// Lecture number 27
+let i = 20;
+//while( i < myState.length ) {
+ //   console.log(myState[i]); 
+ //   i++;
+//} 
+
+//let i = 20;
+do {
+   console.log(myState[i]); 
+    i++;  
+} while( i < 10 );
+
+
